2867022e9e6d56369d6429f001c7337024b47b39
Larry Madeo <lmadeo76@gmail.com>
2017-09-15 05:39:53 +0000

    now using extract_node

--- insert_node.c
+++ insert_node.c
@@ -2,30 +2,27 @@
 
 /**
  * insert_head - inserts new head at head of list ALSO, extract new_head from
-                 previous location
+ *				 previous location
  * @list: double pointer to head of doubly-linked list
  * @new_head: pointer to node that needs to be relocated to head of list
  * TODO: consider joining what was left behind in new_head's old location
  **/
-void insert_head(**list, *new_head)
+void insert_head(listint_s **list, listint_s *new_head)
 {
 	if (list == NULL)
 	{
-        printf("ERROR - null list - insert_head\n");
+		printf("ERROR - null list - insert_head\n");
 		exit;
 	}
 
-    if (new_head->next != NULL)
-		new_head->next->prev = new_head->prev;
-	if (new_head->prev != NULL)
-		node->prev->next = node->next;
+	extract_node(&new_head);
 
-    if (*list == NULL)
+	if (*list == NULL)
 	{
-        *list = new_head;
+		*list = new_head;
 		new_head->prev = new_head->next = NULL;
 		return;
-    }
+	}
 	/* non-edge case */
 	*list->prev = new_head;
 	new_head->next = *list;
@@ -38,10 +35,12 @@ void insert_head(**list, *new_head)
  *                and next nodes.
  * @node: pointer to a node in a double linked list
  **/
-void extract_node(*node)
+void extract_node(listint_s **node)
 {
-    if (new_head->next != NULL)
-		new_head->next->prev = new_head->prev;
-	if (new_head->prev != NULL)
-		node->prev->next = node->next;
+	if (*node->next != NULL)
+		(*node)->next->prev = (*node)->prev;
+	if ((*node)->prev != NULL)
+		(*node)->prev->next = (*node)->next;
+    (*node)->next = NULL;
+    (*node)->prev = NULL;
 }
