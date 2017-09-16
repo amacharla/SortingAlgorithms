9069542e56f5e3226f8144e9538c9685f8f9a464
amacharla <noop2016@gmail.com>
2017-09-16 00:33:24 +0000

    2 done

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,20 +5,26 @@
  * @array: pointer to array of ints
  * @size: size of array
  **/
-
 void selection_sort(int *array, size_t size)
 {
-	size_t j, idx, min_idx = 0;
+	size_t j, idx, min_idx;
 	int tmp;
 
-	for (j = 0; j < size; j++)
+	if (array == NULL)
+		return;
+
+	for (j = 0; j < size; j++) /* go through array */
 	{
-		for (idx = j; idx < size; idx++)
-			if (array[idx] < array[min_idx])
+		min_idx = j; /* numbers before j have already been sorted */
+		for (idx = j; idx < size; idx++) /* look for next min value */
+			if (array[idx] < array[min_idx]) /*update min position*/
 				min_idx = idx;
-		tmp = array[j];
-		array[j] = array[min_idx];
-		array[min_idx] = tmp;
-		print_array(array, size);
+		if (min_idx != j) /* Dont swap in place */
+		{ 	/* put minimum value first */
+			tmp = array[j];
+			array[j] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
 	}
 }
