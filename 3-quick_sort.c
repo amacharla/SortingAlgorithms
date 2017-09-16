2a14eca5f868a8ff467ae888866919700cc97e39
amacharla <noop2016@gmail.com>
2017-09-16 00:02:14 +0000

    2 and 3 workin in progress

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,46 +3,46 @@
 void quick_sort(int *array, size_t size)
 {
 	unsigned int i, j;
-	int pivot, left, right;
+	int pivot, left, right, tmp;
 
 	if (size <= 1 || array == NULL)
 		return;
-
-	for (i = 0; i < size - 1; i++)
-		if (array[i] > array[i + 1])
+	while (1)
+	{
+		for (i = 0; i < size - 1; i++)
+			if (array[i] > array[i + 1])
+				break;
+		if (i == size - 1)
 			break;
-	if (i == size - 1)
-		return;
 
-	pivot = array[size - 1];
-	right = array[size - 2];
+		pivot = array[size - 1];
+		right = array[size - 2];
 
-	for (i = 0; i < size - 1; i++)
-	{
-		left = array[i];
-		if (left >= pivot || left == right)
-			break;
+		for (i = 0; i < size; i++)
+		{
+			left = array[i];
+			if (left >= pivot || left == right)
+				break;
+		}
+
+		for (j = size - 2; j > 0; j--)
+		{
+			right = array[j];
+			if (right < pivot || right == left)
+				break;
+		}
+
+		if (i == j)
+		{
+			array[i] = pivot;
+			array[size - 1] = left;
+		}
+		else
+		{
+			array[i] = right;
+			array[j] = left;
+		}
+
+		print_array(array, size);
 	}
-
-	for (j = size - 2; j > 0; j--)
-	{
-		right = array[j];
-		if (right < pivot || right == left)
-			break;
-	}
-
-	if (i == j)
-	{
-		array[i] = pivot;
-		array[size - 1] = left;
-	}
-	else
-	{
-		array[i] = right;
-		array[j] = left;
-	}
-
-	print_array(array, size);
-
-	quick_sort(array, size);
 }
